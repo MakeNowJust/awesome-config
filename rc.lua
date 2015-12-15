def657a048ec53d8b790ef1d39e84da02abae60a
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-12-15 19:29:00 +0900

    Add autofocus

--- rc.lua
+++ rc.lua
@@ -2,6 +2,7 @@
 local gears = require("gears")
 local awful = require("awful")
 awful.rules = require("awful.rules")
+require("awful.autofocus")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
