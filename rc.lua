d9f196c10babd4f8bd4f75e22ef50c0e0f731e9d
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-12-29 20:00:59 +0900

    Mod+Shift+Space toggle

--- rc.lua
+++ rc.lua
@@ -223,9 +223,7 @@ globalkeys = awful.util.table.join(
   awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
   awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
   awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-  --awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-  awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, 1) end),
-
+  awful.key({ modkey, "Shift"   }, "Space", function () awful.layout.inc(layouts, 1) end),
 
   -- プロンプトの起動
   awful.key({ modkey            }, "r",     function () mypromptbox[mouse.screen]:run() end),
