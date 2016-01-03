fce0d9ec3189af1dadd64a533c19fffa3e58e1e9
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-01-03 10:18:01 +0900

    Add screen shot

--- rc.lua
+++ rc.lua
@@ -235,11 +235,11 @@ globalkeys = awful.util.table.join(
 
   -- PrintScreenでスクショを撮る
   awful.key({                   }, "Print", function ()
-    awful.util.spawn("scrot '%Y%m%d%H%M%S.png' -e 'mv $f ~/Pictures/screenshot/'", false)
+    awful.util.spawn("scrot '%Y%m%d%H%M%S.png' -e 'mv $f ~/Pictures/ScreenShot/'")
   end),
   -- Alt+PrintScreenで現在のウィンドウのスクショ
   awful.key({ "Mod1"            }, "Print", function ()
-    awful.util.spawn("scrot '%Y%m%d%H%M%S.png' -e 'mv $f ~/Pictures/screenshot/'", false)
+    awful.util.spawn("scrot -u '%Y%m%d%H%M%S.png' -e 'mv $f ~/Pictures/ScreenShot/'")
   end)
 )
 
