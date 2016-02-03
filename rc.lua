245953bd774d4389b8ac5401727711900368e3f8
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-02-04 08:10:41 +0900

    Use UTF-8

--- rc.lua
+++ rc.lua
@@ -51,10 +51,7 @@ awful.util.spawn_with_shell("pgrep dropbox || dropbox")
 beautiful.init(awful.util.getdir("config") .. "/theme.lua")
 
 -- 端末とエディタ
-terminal = "mlterm"
-editor = "vim"
-editor_cmd = terminal .. " -e " .. editor
-
+terminal = "env LANG=en_US.UTF_8 mlterm"
 modkey = "Mod4"
 
 -- レイアウトの種類
